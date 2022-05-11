2a591fdee6617793ee6f152e6128f5087d27a277
Leonardo Aguiar <leonardoaguiar1235@gmail.com>
2022-05-11 03:56:18 -0300

    POO

---
+++ Curso em video1/src/br/com/cursoemvideo/aula7/Publicacao.java
@@ -0,0 +1,10 @@
+package br.com.cursoemvideo.aula7;
+
+public interface Publicacao {
+	public void abrir();
+	public void fechar();
+	public void folhear(int p);
+	public void avancarpag();
+	public void voltarpag();
+	
+}
